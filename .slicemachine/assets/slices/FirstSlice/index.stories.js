3c91979f0460fbcd16a0ce032a7d9efd4a94bebb
Carl Serquina <carl.serquina@flexisourceit.com.au>
2022-06-01 17:07:59 +0800

    populate data

---
+++ .slicemachine/assets/slices/FirstSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/FirstSlice';
+
+export default {
+  title: 'slices/FirstSlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"first_slice","items":[],"primary":{"title":[{"type":"heading1","text":"Cultivate intuitive models","spans":[]}],"description":[{"type":"paragraph","text":"Dolor aliqua consectetur proident velit elit ut sunt consequat culpa.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
